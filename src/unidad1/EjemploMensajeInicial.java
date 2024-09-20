afcd0c81aee97a2331ecd62c50eb0d402b7d9c0d
blame110 <46011863+blame110@users.noreply.github.com>
2024-09-20 11:37:18 +0200

    Initial commit

---
+++ src/unidad1/EjemploMensajeInicial.java
@@ -0,0 +1,12 @@
+package unidad1;
+
+public class EjemploMensajeInicial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.print("Primer Mensaje");
+		    
+	}
+
+}
